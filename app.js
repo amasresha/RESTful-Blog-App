b40b26c399601e307353baf34e2823b0d7e6ee9a
amasresha <a.masresha@gmail.com>
2018-05-07 15:07:48 -0700

    removed express-sanitizer as page load is pending indefinitely

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var methodoverride = require("method-override");
-var expressSanitizer = require("express-sanitizer");
+// var expressSanitizer = require("express-sanitizer");
+
 
 var app = express();
 var port = 4500;
@@ -12,7 +13,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressSanitizer);
+// app.use(expressSanitizer);
 app.use(methodoverride("_method")); // this is used for PUT and DELETE request
 
 //mongoose/model config
@@ -60,7 +61,7 @@ app.get("/blogs/new", function(req, res) {
 //CREATE ROUTE
 app.post("/blogs", function(req, res) {
     //create blog
-    req.body.blog.body = req.sanitize(req.body.blog.body);
+    // req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog) {
         if (err) {
             res.render("new");
@@ -95,7 +96,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
-    req.body.blog.body = req.sanitize(req.body.blog.body);
+    // req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
         if (err) {
             res.redirect("/blogs");
