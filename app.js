62e72bb755c8cc9873451fc4a0bd136d495d83d0
amasresha <a.masresha@gmail.com>
2018-05-07 11:32:15 -0700

    style pages and add PUT and DELETE requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var methodoverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
+
 var app = express();
 var port = 4500;
 
@@ -9,6 +12,8 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer);
+app.use(methodoverride("_method")); // this is used for PUT and DELETE request
 
 //mongoose/model config
 var blogSchema = new mongoose.Schema({
@@ -52,8 +57,10 @@ app.get("/blogs/new", function(req, res) {
     res.render("new");
 });
 
+//CREATE ROUTE
 app.post("/blogs", function(req, res) {
     //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog) {
         if (err) {
             res.render("new");
@@ -76,8 +83,38 @@ app.get("/blogs/:id", function(req, res) {
 });
 
 //EDIT ROUTE
-app.get("./blogs/:id/edit", function(req, res) {
-    res.render("edit");
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", { blog: foundBlog });
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+
 });
 app.listen(port, function() {
     console.log("Server is running");
